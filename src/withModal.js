7203966337b7b7fc93b78e7a5e76a901dee67e2e
Diego Muracciole <diegomuracciole@gmail.com>
2018-02-25 02:07:32 -0300

    Implement withModal

--- src/withModal.js
+++ src/withModal.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const withModal = (
-  modalType,
-  callbackName,
-) => WrappedComponent => props => {
-  const callback = {
-    [callbackName]: props.showModal.bind(null, modalType),
+export const withModal = (modalType, callbackName) => WrappedComponent => {
+  const Wrapper = (props, context) => {
+    const callback = {
+      [callbackName || 'showModal']: context.showModal.bind(null, modalType),
+    };
+
+    return React.createElement(
+      WrappedComponent,
+      Object.assign({}, props, callback),
+    );
   };
 
-  return React.createElement(
-    WrappedComponent,
-    Object.assign({}, props, callback),
-  );
+  Wrapper.contextTypes = {
+    showModal: PropTypes.func,
+  };
+
+  return Wrapper;
 };

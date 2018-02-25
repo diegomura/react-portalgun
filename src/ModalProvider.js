8028c26b8dd22645c01eb305c83361f353fb2689
Diego Muracciole <diegomuracciole@gmail.com>
2018-02-25 02:10:27 -0300

    Use regular Component for ModalProvider

--- src/ModalProvider.js
+++ src/ModalProvider.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export class ModalProvider extends React.PureComponent {
+export class ModalProvider extends React.Component {
   constructor(props) {
     super(props);
 

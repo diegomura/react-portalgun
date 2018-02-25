ac126be614fcf4b92349063dd9a1cc6206eb6a35
Diego Muracciole <diegomuracciole@gmail.com>
2018-02-25 02:22:34 -0300

    Support mapModalProps

--- src/withModal.js
+++ src/withModal.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const withModal = (modalType, callbackName) => WrappedComponent => {
+export const withModal = (
+  modalType,
+  callbackName,
+  mapModalProps,
+) => WrappedComponent => {
   const Wrapper = (props, context) => {
-    const callback = {
-      [callbackName || 'showModal']: context.showModal.bind(null, modalType),
-    };
+    let callback;
+
+    if (mapModalProps) {
+      callback = event => {
+        const modalProps = mapModalProps({ event, ownProps: props });
+        return context.showModal(modalType, modalProps);
+      };
+    } else {
+      callback = context.showModal.bind(null, modalType);
+    }
 
     return React.createElement(
       WrappedComponent,
-      Object.assign({}, props, callback),
+      Object.assign({}, props, { [callbackName || 'showModal']: callback }),
     );
   };
 

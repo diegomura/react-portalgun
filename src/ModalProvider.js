56b2ec0abe423c3d4b312bf376b48bef44613366
Diego Muracciole <diegomuracciole@gmail.com>
2018-02-25 01:57:07 -0300

    Implement ModalProvider

--- src/ModalProvider.js
+++ src/ModalProvider.js
@@ -1,3 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export class ModalProvider extends React.PureComponent {}
+export class ModalProvider extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalType: null,
+      modalProps: {},
+    };
+  }
+
+  getChildContext() {
+    return { showModal: this.showModal.bind(this) };
+  }
+
+  showModal(type, props) {
+    this.setState({ modalType: type, modalProps: props });
+  }
+
+  closeModal() {
+    this.setState({ modalType: null, modalProps: {} });
+  }
+
+  render() {
+    const { modals } = this.props;
+    const { modalType } = this.state;
+    const ModalComponent = modals[modalType];
+
+    return React.createElement(
+      'div',
+      null,
+      ModalComponent &&
+        React.createElement(ModalComponent, {
+          onClose: this.closeModal.bind(this),
+        }),
+      this.props.children,
+    );
+  }
+}
+
+ModalProvider.childContextTypes = {
+  showModal: PropTypes.func,
+};
